1d01e6895eb6260c7e864f3fc4792ae74416b991
Tong Lin <wowawawoa@gmail.com>
2024-01-30 16:01:56 +1030

    chrome: adding head and favicon

---
+++ app/head.tsx
@@ -0,0 +1,31 @@
+export default function Head() {
+  return (
+    <>
+      <title>Tong Lin</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Tong Lin's Portfolio" />
+      <meta
+        name="keywords"
+        content="Tong Lin, Portfolio, Full stack developer, Frontend developer"
+      />
+      <meta name="author" content="Tong Lin" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
