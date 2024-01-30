6a7b2493d5e93bc5edb4962ac2bca31697a81203
Tong Lin <wowawawoa@gmail.com>
2024-01-30 14:25:56 +1030

    feat: adding about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
---
+++ components/AboutSection.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "PHP" },
+  { skill: "Git" },
+  { skill: "Node.js" },
+];
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Tong and I am a{" "}
+              <span className="font-bold">{"quality-focused"}</span>, and
+              <span className="font-bold">{" self-motivated"}</span> full stack
+              developer.
+            </p>
+            <br />
+            <p>
+              I am deeply passionate about developing web applications through
+              the execution of projects, successfully navigating challenging
+              tasks, perpetually enhancing and acquiring new skills, fostering
+              meaningful relationships with clients and peers, and championing a
+              collaborative team environment within the organization.
+            </p>
+            <br />
+            <p>
+              I always strive to learn and grow because I love technology and
+              enjoy pushing the limits of what I can do. I&apos;m excited about
+              where my career will take me and am open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
