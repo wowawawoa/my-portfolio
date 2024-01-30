6629e3817c39f2ad2f23bf4e4d9d212386ea079b
Tong Lin <wowawawoa@gmail.com>
2024-01-30 11:29:44 +1030

    feat: adding navbar layout, light/dark mode

---
+++ components/Navbar.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
+interface NavItem {
+  label: string;
+  page: string;
+}
+
+const NAV_ITEMS: NavItem[] = [
+  { label: "Home", page: "home" },
+  { label: "About", page: "about" },
+  { label: "Projects", page: "projects" },
+];
+
+const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navbar, setNavbar] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a>
+              <div className="container flex items-center space-x-2">
+                <h2 className="text-2xl font-bold">Tong Lin</h2>
+              </div>
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <a
+                    key={idx}
+                    className={
+                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                    }
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </a>
+                );
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
